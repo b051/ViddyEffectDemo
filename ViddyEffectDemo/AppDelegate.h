ff4d1d85f8b62d2da54dc7962b0061ff9fe45a45
Rex Sheng <shengning@gmail.com>
2012-10-30 17:01:21 +0800

    Initial Commit

---
+++ ViddyEffectDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViddyEffectDemo
+//
+//  Created by Rex Sheng on 10/30/12.
+//  Copyright (c) 2012 Rex.S Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViddyEffectDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViddyEffectDemo
+//
+//  Created by Rex Sheng on 10/30/12.
+//  Copyright (c) 2012 Rex.S Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
