ca5f4d2fe5b9481e2c02e041bcfc4915dd49087d
Rex Sheng <shengning@gmail.com>
2012-10-30 18:13:28 +0800

    init commit. first two effects now finished

---
+++ ViddyEffectDemo/ViddyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViddyViewController.h
+//  FilterShowcase
+//
+//  Created by Rex Sheng on 10/30/12.
+//  Copyright (c) 2012 Cell Phone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViddyViewController : UIViewController
+
+@end
---
+++ ViddyEffectDemo/ViddyViewController.m
@@ -0,0 +1,135 @@
+//
+//  ViddyViewController.m
+//  FilterShowcase
+//
+//  Created by Rex Sheng on 10/30/12.
+//  Copyright (c) 2012 Cell Phone. All rights reserved.
+//
+
+#import "ViddyViewController.h"
+#import "iCarousel.h"
+#import <GPUImage/GPUImage.h>
+
+@interface ViddyViewController () <iCarouselDataSource, iCarouselDelegate>
+
+@end
+
+@implementation ViddyViewController
+{
+	GPUImageVideoCamera *videoCamera;
+	GPUImageFilterPipeline *pipeline;
+	__unsafe_unretained GPUImageView *filterView;
+	NSArray *effects;
+	NSArray *effectsConfiguration;
+}
+
+- (void)sliderValueChanged:(UISlider *)slider
+{
+	
+}
+
+- (void)selectVintage:(id)sender
+{
+	
+}
+
+#pragma mark - View Lifecycle
+- (void)loadView
+{
+	GPUImageView *view = [[GPUImageView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+	view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+	self.view = filterView = view;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	CGSize size = self.view.bounds.size;
+	effects = @[@"vintage", @"soho", @"bw"];
+	NSDictionary *conf = @{@"Filters" : @[
+	@{@"FilterName" : @"GPUImageSepiaFilter"},
+	@{@"FilterName" : @"GPUImagePixellateFilter", @"Attributes" : @{@"setFractionalWidthOfAPixel:" : @"float(0.05)"}}
+	]};
+
+	NSDictionary *vintage = @{@"Filters" : @[
+	@{@"FilterName" : @"GPUImageSketchFilter"}
+	]};
+	NSDictionary *soho = @{@"Filters" : @[
+	@{@"FilterName" : @"GPUImageVignetteFilter"}
+	]};
+	NSDictionary *bw = @{@"Filters" : @[
+	@{@"FilterName" : @"GPUImageGrayscaleFilter"}
+	]};
+	effectsConfiguration = @[vintage, soho, bw];
+	
+	UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(15, size.height - 100, size.width - 30, 20)];
+	slider.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
+	[slider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+	[self.view addSubview:slider];
+	
+	iCarousel *carousel = [[iCarousel alloc] initWithFrame:CGRectMake(0, size.height - 80, size.width, 80)];
+	carousel.dataSource = self;
+	carousel.delegate = self;
+	carousel.centerItemWhenSelected = NO;
+	carousel.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
+	[self.view addSubview:carousel];
+	
+	videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+	videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+	
+	GPUImageGrayscaleFilter *filter = [[GPUImageGrayscaleFilter alloc] init];
+	[videoCamera addTarget:filter];
+	[filter addTarget:filterView];
+	
+	pipeline = [[GPUImageFilterPipeline alloc] initWithConfiguration:effectsConfiguration[0]
+															   input:videoCamera
+															  output:filterView];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:animated];
+	[videoCamera startCameraCapture];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [videoCamera stopCameraCapture];
+	[super viewWillDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+{
+	return toInterfaceOrientation == UIInterfaceOrientationPortrait;
+}
+
+#pragma mark - iCarousel Delegate & DataSource
+- (void)carousel:(iCarousel *)carousel didSelectItemAtIndex:(NSInteger)index
+{
+	[videoCamera startCameraCapture];
+	pipeline = [[GPUImageFilterPipeline alloc] initWithConfiguration:effectsConfiguration[index]
+															   input:videoCamera
+															  output:filterView];
+	[videoCamera startCameraCapture];
+}
+
+- (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
+	return effects.count;
+}
+
+- (CGFloat)carouselItemWidth:(iCarousel *)carousel
+{
+	return 90;
+}
+
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSUInteger)index reusingView:(UIView *)view
+{
+	if (!view) {
+		view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 70, 70)];
+	}
+	[(UIImageView *)view setImage:[UIImage imageNamed:[NSString stringWithFormat:@"%@.png", effects[index]]]];
+	return view;
+}
+
+@end
